395c2345435cbe1a85bf236dbc646dc6bc415925
Your Name <you@example.com>
2021-02-11 07:57:05 +0700

    First Commit

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -16,7 +16,7 @@ const Register = ({ history }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(process.env.REACT_APP_REGISTER_REDIRECT_URL);
+    // console.log(process.env.REACT_APP_REGISTER_REDIRECT_URL);
     const config = {
       url: process.env.REACT_APP_REGISTER_REDIRECT_URL,
       handleCodeInApp: true,
